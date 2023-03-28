4d0a18075930ae23271a86704a9fab880d09d305
liujuncai <1769031928@qq.com>
2023-03-28 15:02:11 +0800

    [Feat] add ssl book

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
-
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import * as http from 'http';
+import * as https from 'https';
 async function bootstrap() {
   const httpsOptions = {
     ca: fs.readFileSync('./utils/ssl/7802960_www.flyfrag.cn_chain.crt'),
     key: fs.readFileSync('./utils/ssl/7802960_www.flyfrag.cn.key'),
     cert: fs.readFileSync('./utils/ssl/7802960_www.flyfrag.cn_public.crt'),
   };
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
+
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.enableCors();
+  await app.init();
 
-  await app.listen(3000);
+  http.createServer(server).listen(3000);
+  https.createServer(httpsOptions, server).listen(443);
+  // await app.listen(3000);
 }
 bootstrap();
