450e418dd287503d5621b43df43a50a7f220b824
liujuncai <1769031928@qq.com>
2023-03-28 14:50:11 +0800

    [Feat] add ssl book

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    ca: fs.readFileSync('./utils/ssl/7802960_www.flyfrag.cn_chain.crt'),
+    key: fs.readFileSync('./utils/ssl/7802960_www.flyfrag.cn.key'),
+    cert: fs.readFileSync('./utils/ssl/7802960_www.flyfrag.cn_public.crt'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
   app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
