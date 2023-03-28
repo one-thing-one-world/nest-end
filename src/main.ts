92e2d5f8b96f4efae05aa80ab75c6ad4212989ec
liujuncai <1769031928@qq.com>
2023-03-28 15:44:20 +0800

    [Feat] 配置ssl

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
+import * as express from 'express';
 import * as http from 'http';
 import * as https from 'https';
+import { join } from 'path';
 async function bootstrap() {
+  // const apiSecret = fs
+  //   .readFileSync(
+  //     join(process.cwd(), './utils/ssl/7802960_www.flyfrag.cn_public.crt'),
+  //   )
+  //   .toString();
+  console.log(process.cwd(), '/src/utils/ssl/7802960_www.flyfrag.cn_chain.crt');
   const httpsOptions = {
-    ca: fs.readFileSync('./utils/ssl/7802960_www.flyfrag.cn_chain.crt'),
-    key: fs.readFileSync('./utils/ssl/7802960_www.flyfrag.cn.key'),
-    cert: fs.readFileSync('./utils/ssl/7802960_www.flyfrag.cn_public.crt'),
+    ca: fs
+      .readFileSync(
+        join(process.cwd(), '/src/utils/ssl/7802960_www.flyfrag.cn_chain.crt'),
+      )
+      .toString(),
+    key: fs
+      .readFileSync(
+        join(process.cwd(), '/src/utils/ssl/7802960_www.flyfrag.cn.key'),
+      )
+      .toString(),
+    cert: fs
+      .readFileSync(
+        join(process.cwd(), '/src/utils/ssl/7802960_www.flyfrag.cn_public.crt'),
+      )
+      .toString(),
   };
 
   const server = express();
